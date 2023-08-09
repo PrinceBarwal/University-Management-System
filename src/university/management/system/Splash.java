884e17ea178ae4b6115fdfda2eb9ff0850321834
Prince Saini <princesaini0144@gmail.com>
2023-08-10 00:09:54 +0530

    Added splash file

--- src/university/management/system/Splash.java
+++ src/university/management/system/Splash.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package university.management.system;
+import javax.swing.*;
+import java.awt.*;
 
-/**
- *
- * @author princ
- */
-public class Splash {
+
+
+public class Splash extends JFrame implements Runnable{
     
+    Thread t;
+    
+    
+    Splash(){
+//        setSize(500,500);
+//        setLocationRelativeTo(null);
+//        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/first.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+        
+        
+        t = new Thread(this);
+        t.start();
+        
+        setVisible(true);
+        
+        int x = 1;
+        
+        for(int i=2 ; i<=580 ; i+=4 , x+=1){
+            setLocation(530 - ((i+x)/2) , 330 - (i/2));
+            setSize(i+3*x , i+x/2);
+            
+            try{
+                Thread.sleep(10);
+                
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void run(){
+        try{
+            Thread.sleep(7000);
+            setVisible(false);
+            new Login();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String [] args){
+        new Splash();
+    }
 }

db9861ab8300c16ffa34e213f2ca270dff12b30c
Prince Saini <princesaini0144@gmail.com>
2023-08-10 00:11:31 +0530

    Added login file

---
+++ src/university/management/system/Login.java
@@ -0,0 +1,19 @@
+package university.management.system;
+import javax.swing.*;
+
+
+public class Login extends JFrame{
+    
+    Login(){
+        
+        setSize(500 , 500);
+        setLocationRelativeTo(null);
+        
+        
+        setVisible(true);
+    }
+    
+    public static void main(String[] args){
+        new Login();
+    }
+}
